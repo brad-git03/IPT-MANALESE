02c0422db23cd247824d0b99722b20df3eaf7ea4
brad-git03 <bsmanalese.student@ua.edu.ph>
2025-07-15 09:55:02 +0800

    Added Login/Logout

--- session14/task-app/src/App.js
+++ session14/task-app/src/App.js
@@ -1,23 +1,26 @@
 import './App.css';
+import { AuthProvider } from './AuthContext';
 import AppNavbar from './components/AppNavbar';
 import Error from './pages/Error';
 // import Hook1 from './pages/Hook1';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
+import Tasks from './pages/Tasks';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppNavbar/>
-      <Routes>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='*' element={<Error/>}/>
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <AppNavbar />
+        <Routes>
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
